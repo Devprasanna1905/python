a687d4aafe17335371c20944e861525e6192dd0d
Devprasanna1905 <70941315+Devprasanna1905@users.noreply.github.com>
2021-07-28 10:21:50 +0530

    Create linearsearch in python.py

---
+++ linearsearch in python.py
@@ -0,0 +1,12 @@
+def linear(list,n):
+    for i in range(len(list)):
+        if(list[i]==n):
+            return "found"
+    return "not found"
+print(linear([1,2,4,5],1))            
+#for user input 
+#use append
+#list=[]
+#for i in range(0,5):
+   # e=int(input())
+   #list.append(e)
